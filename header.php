61c0e2de93ea36b537e3313ab14784c9ea1e390b
Shane <Shane@essentialmarketer.com>
2016-02-11 15:23:22 +0000

    added in WordPress HTML tags

--- header.php
+++ header.php
@@ -18,20 +18,22 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div class="top-bar">
-	<div class="top-bar-left">
-		<ul class="menu">
-			<li class="menu-text">Marketing Site</li>
-			<li><a href="#">One</a></li>
-			<li><a href="#">Two</a></li>
-		</ul>
+<header id="masthead" class="site-header" role="banner">
+	<div class="top-bar">
+		<div class="top-bar-left">
+			<ul class="menu">
+				<li class="menu-text">Marketing Site</li>
+				<li><a href="#">One</a></li>
+				<li><a href="#">Two</a></li>
+			</ul>
+		</div>
+		<div class="top-bar-right">
+			<ul class="menu">
+				<li><a href="#">Three</a></li>
+				<li><a href="#">Four</a></li>
+				<li><a href="#">Five</a></li>
+				<li><a href="#">Six</a></li>
+			</ul>
+		</div>
 	</div>
-	<div class="top-bar-right">
-		<ul class="menu">
-			<li><a href="#">Three</a></li>
-			<li><a href="#">Four</a></li>
-			<li><a href="#">Five</a></li>
-			<li><a href="#">Six</a></li>
-		</ul>
-	</div>
-</div>
+</header>
